0b400f4042a32ee10bc9bf70c1e915bde7dd8260
EnArvy <darklacerta7817@gmail.com>
2021-02-03 20:31:04 +0530

    Added timer, instructions, 6 blocks and 3 tries

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 
-var t=new Array();
-var counter;
+var t=new Array();                                          //array for each button
+var counter=3;                                              //number of tries
 var backgroundcolor = document.getElementById("header").style.background;
+var sec = 20;
 
-for (var i=1; i<7; i++)
+for (var i=1; i<7; i++)                                    //assigning array to buttons
   t[i]=document.getElementById(i);
 
 var correctColor;
 
-function getRandomColor() {
+function getRandomColor() {                                //function that generates random colour
   var letters = "0123456789ABCDEF";
   var color = "#";
   for (var i = 0; i < 6; i++) 
@@ -16,39 +17,69 @@ function getRandomColor() {
   return color;
 }
 
-function newGame() {
+
+
+function newGame() {                                       //initialising values upon reload
+  console.log(1)
   for ( var i=1; i<7; i++)
     t[i].style.background = getRandomColor();  
   x = Math.floor(Math.random() * 6 + 1);
   correctColor=t[x].style.background;
-  document.getElementById("headerClue").innerHTML = correctColor;
-  counter = 2;
+  document.getElementById("clue").innerHTML = correctColor;
+  counter = 3;
   document.getElementById("counter").innerHTML = counter;
   document.getElementById("header").style.background = backgroundcolor;
 }
 
-var NewGame = document.getElementById("NewGame");
-NewGame.addEventListener("click", function(){ newGame();})
+var NewGame = document.getElementById("NewGame");            //new game button
+NewGame.addEventListener("click", function(){ location.reload();})
 
 
 
-for ( let j=1; j<7; j++){
-  t[j].addEventListener("click", function () {
-    if( t[j].style.background == correctColor){
+for ( let j=1; j<7; j++){                                        //clicking buttons and logic
+  t[j].addEventListener("click", function(){
+    if(t[j].style.background == correctColor && counter!=0)
+    {
       document.getElementById("header").style.background = correctColor;
-      document.getElementById("counter").innerHTML = "You Won, Click New Game to play again";
-    }  
+      document.getElementById("headerClue").style.background = correctColor;
+      document.getElementById("NewGame").style.background = correctColor;
+      document.getElementById("Timer").style.background = correctColor;
+      document.getElementById("status").innerHTML = "You Won, Click New Game to play again";
+      counter=0;
+      document.getElementById("counter").innerHTML="";
+      sec=NaN;
+    }
     else{
-      document.getElementById("header").style.background = "black";
       counter--;
+      t[j].style.background = "black";
+      this.removeEventListener("click",arguments.callee);
+      if(counter==0){
+        document.getElementById("status").innerHTML = "You Lost, Click New Game to play again";
+        document.getElementById("counter").innerHTML="";
+        sec=NaN;
+      }  
+      sec=20;
     }
     document.getElementById("counter").innerHTML = counter;
-    if(counter==0)
-      document.getElementById("counter").innerHTML = "You Lost, Click New Game to play again";
-    if(counter==-1)
-      document.getElementById("body").onclick=newGame();
-    
-   }  
-  )
+
+  if(counter==-1)
+    document.getElementById("body").onclick=location.reload();
+  }
+  );
+ 
+} 
+     
+
+window.onload = function(){                    //Timer
+  newGame();
+  setInterval(function(){
+    document.getElementById("sec").innerHTML = sec;
+    sec--;
+    if(sec == 0){
+      sec = 20;
+      counter--;
+      document.getElementById("counter").innerHTML=counter;
+      if(counter == 0) location.reload();
+    }
+  },1000);
 }
-      
